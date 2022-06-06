0998d3c953d45e2dd17ffcb6f1b52cb95ca71f7f
junior SANCA <juniorsanca@MacBook-Pro-de-junior.local>
2022-06-06 06:34:02 +0200

    add icon inside image and change background to grey and borderradius to 50

--- src/screens/components/In.js
+++ src/screens/components/In.js
@@ -4,7 +4,7 @@ import {
     View,
     Image,
     StyleSheet,
-
+    Pressable,
 } from 'react-native';
 
 
@@ -16,12 +16,20 @@ const In = (props) => {
 
     const{ image, title} = props.in;
 
+    const onLike = () => {};
+
     return (
         <View style={styles.in}>
-            <Image source={{ uri: image }} style={styles.in_image}/>
-            <Icon name="heart" size={50} color="red" />
+            <View>
 
-            <Text style={styles.in_name}>{title}</Text>
+                <Image source={{ uri: image }} style={ styles.in_image }/>
+                    
+                <Pressable onPress={onLike} style={styles.heartBtns}>
+                    <Icon name="hearto" size={20} color="black" />
+                </Pressable>
+
+            </View>
+                <Text style={styles.in_name}>{title}</Text>
         </View>
     )
 }
@@ -37,13 +45,22 @@ const styles = StyleSheet.create({
     },
     in_image: {
         borderRadius: 10,
-        width: 350, height: '55%'
+        width: 350, height: 200
     },
     in_name: {
         fontSize: 20,
         textAlign: "center",
         margin: 5,
     },
+    heartBtns: {
+        backgroundColor: '#D3CFD4',
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+        padding: 5,
+        borderRadius: 50
+
+    }
 
 })
 
